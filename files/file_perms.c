7555ebcdb24e0e6d7881835ad30da72d9ff11263
Steven Xiao <xiaoxi32@gmail.com>
2020-04-27 14:39:26 +0800

    rm ycm config file

--- files/file_perms.c
+++ files/file_perms.c
@@ -1,21 +1,18 @@
-#include <sys/stat.h>
-#include <stdio.h>
 #include "file_perms.h"
+#include <stdio.h>
+#include <sys/stat.h>
 
 #define STR_SIZE sizeof("rwxrwxrwx")
-
 
 char *filePermStr(mode_t mode, int flags)
 {
   static char str[STR_SIZE];
   snprintf(str, STR_SIZE, "%c%c%c%c%c%c%c%c%c",
-      (mode & S_IRUSR)?'r':'-', (mode & S_IWUSR)?'w':'-', 
-      (mode & S_IXUSR)?(((mode & S_ISUID) && (flags & FP_SPECIAL))?'s':'x'):
-      (((mode & S_ISUID) && (flags & FP_SPECIAL))?'S':'-'),
-      (mode & S_IRGRP)?'r':'-', (mode & S_IWGRP)?'w':'-',
-      (mode & S_IXGRP)?(((mode & S_ISGID) && (flags & FP_SPECIAL))? 's':'x'):(((mode & S_ISGID) && (flags & FP_SPECIAL))?'S':'-'),
-      (mode & S_IROTH)?'r':'-',(mode & S_IWOTH)?'w':'-',
-      (mode & S_IXOTH)?(((mode & S_ISVTX) && (flags & FP_SPECIAL))?'t':'x'):(((mode & S_ISVTX) && (flags & FP_SPECIAL))?'T':'-'));
+           (mode & S_IRUSR) ? 'r' : '-', (mode & S_IWUSR) ? 'w' : '-',
+           (mode & S_IXUSR) ? (((mode & S_ISUID) && (flags & FP_SPECIAL)) ? 's' : 'x') : (((mode & S_ISUID) && (flags & FP_SPECIAL)) ? 'S' : '-'),
+           (mode & S_IRGRP) ? 'r' : '-', (mode & S_IWGRP) ? 'w' : '-',
+           (mode & S_IXGRP) ? (((mode & S_ISGID) && (flags & FP_SPECIAL)) ? 's' : 'x') : (((mode & S_ISGID) && (flags & FP_SPECIAL)) ? 'S' : '-'),
+           (mode & S_IROTH) ? 'r' : '-', (mode & S_IWOTH) ? 'w' : '-',
+           (mode & S_IXOTH) ? (((mode & S_ISVTX) && (flags & FP_SPECIAL)) ? 't' : 'x') : (((mode & S_ISVTX) && (flags & FP_SPECIAL)) ? 'T' : '-'));
   return str;
 }
-
